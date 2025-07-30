e99378e40ac286064b20ec4b711550cc74d6e123
coroni19 <roni@combina.co.il>
2025-07-30 15:25:03 +0300

    triger

--- src/config/DBValidation.config.ts
+++ src/config/DBValidation.config.ts
@@ -4,6 +4,6 @@ export const validationSchema = Joi.object({
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432),
   DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
 });
